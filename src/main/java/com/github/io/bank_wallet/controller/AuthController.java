0db993abe28f72c6ff21a5bc615932aed2e4229b
Halkano Malise <kudezx@gmail.com>
2025-03-09 13:52:37 +0300

    updated Response for authentication

---
+++ src/main/java/com/github/io/bank_wallet/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.github.io.bank_wallet.controller;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.io.bank_wallet.dto.AuthRequest;
+import com.github.io.bank_wallet.entity.User;
+import com.github.io.bank_wallet.enums.Roles;
+import com.github.io.bank_wallet.response.AuthResponse;
+import com.github.io.bank_wallet.service.UserService;
+import com.github.io.bank_wallet.utils.JwtUtil;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest authRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.username(), authRequest.password())
+        );
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.username());
+        String token = jwtUtil.generateToken(userDetails);
+
+        // Extract role from the user details (casting to User to access the role)
+        Roles role = ((User) userDetails).getRole(); // Assuming User class has getRole method
+
+
+        return new AuthResponse(token, userDetails.getUsername(), role);
+    }
+    
+}

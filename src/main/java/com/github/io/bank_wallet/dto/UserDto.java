39ae3ba45a7db07771cbcac33d56e9496bfe0a2e
Halkano Malise <kudezx@gmail.com>
2025-03-09 14:37:39 +0300

    update the paramater for role to set of roles

--- src/main/java/com/github/io/bank_wallet/dto/UserDto.java
+++ src/main/java/com/github/io/bank_wallet/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.github.io.bank_wallet.dto;
 
+import java.util.Set;
+
 import com.github.io.bank_wallet.enums.Roles;
 
 import lombok.Builder;
@@ -9,5 +11,5 @@ import lombok.Builder;
 // }
 
 @Builder
-public record UserDto(String firstName, String lastName, String username, String email, Roles role, boolean isVerified) {
+public record UserDto(String firstName, String lastName, String username, String email, Set<Roles> role, boolean isVerified) {
 }

ca9e7dbd91dbbe87984836da4a1567ffe93ba1d5
Halkano Malise <kudezx@gmail.com>
2025-03-09 11:26:51 +0300

    implemented userdetails from speing security and jwt

--- src/main/java/com/github/io/bank_wallet/entity/User.java
+++ src/main/java/com/github/io/bank_wallet/entity/User.java
@@ -2,6 +2,13 @@ package com.github.io.bank_wallet.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.mapping.UserDefinedType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.github.io.bank_wallet.enums.Roles;
 
@@ -26,7 +33,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,7 +45,7 @@ public class User {
     @Column(nullable = false)
     private String lastname;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String username;
 
     @Column(unique = true, nullable = false)
@@ -69,4 +76,41 @@ public class User {
     protected void onUpdate() {
         updatedAt = LocalDateTime.now();
     }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name())); // Add "ROLE_" prefix
+        }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username; // We authenticate using username
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isVerified; // Only allow verified users to log in
+    }
+
 }

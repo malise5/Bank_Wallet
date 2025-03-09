234408550b47e3ca54fa206980926a0378608975
Halkano Malise <kudezx@gmail.com>
2025-03-09 08:33:31 +0300

    Add findByWalletId method to TransactionRepository

---
+++ src/main/java/com/github/io/bank_wallet/repository/TransactionRepository.java
@@ -0,0 +1,16 @@
+package com.github.io.bank_wallet.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.io.bank_wallet.entity.Transaction;
+import com.github.io.bank_wallet.enums.StatusType;
+import com.github.io.bank_wallet.enums.TransactionType;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByWalletId(Long walletId);
+    List<Transaction> findByTransactionsType(TransactionType transactionType);
+    List<Transaction> findByStatus(StatusType status);
+}
